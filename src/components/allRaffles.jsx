9c5de6cde8a6cdad705cb2379891bfd06c082d44
Bayram Çınar <bayram200212@gmail.com>
2023-12-12 14:01:46 +0300

    getGiveawayInfo

--- src/components/allRaffles.jsx
+++ src/components/allRaffles.jsx
@@ -16,31 +16,130 @@ const contractAddress = "0x2c517B6B6819c70feE135519fBa8CbF1aA43277c";
 
 function AllRaffles() {
   const { walletProvider } = useWeb3ModalProvider()
-  //const { address, chainId, isConnected } = useWeb3ModalAccount()
-
-  try {
-
-  } catch (e) { console.log(e) }
-
+  const { address, chainId, isConnected } = useWeb3ModalAccount()
+  const [raffle1, setRaffle1] = useState(null);
 
   async function getGiveaway(id) { // return array of giveaway.
     try {
       const ethersProvider = new ethers.providers.Web3Provider(walletProvider)
       const contract = new ethers.Contract(contractAddress, ABI, ethersProvider);
       const giveaway = await contract.getGiveaway(id);
+      const name = giveaway["giveawayName"];
       return giveaway;
     } catch (e) { console.log(e) }
   }
 
-  async function getLiveGiveaways() { // return array of liveaways ids.
-      //if(!isConnected) throw Error("User disconnected");
+  async function getGiveawayMaxParticuls(id) { // max katılımcı sayısı.
+    try {
       const ethersProvider = new ethers.providers.Web3Provider(walletProvider)
       const contract = new ethers.Contract(contractAddress, ABI, ethersProvider);
-      const liveGiveaways = await contract.getLiveGiveaways();
-      return liveGiveaways;
-    
+      const giveaway = await contract.getGiveaway(id);
+      const number = giveaway["maxParticipants"];
+      return converToDeciaml(number._hex);
+    } catch (e) { console.log(e) }
   }
-  getLiveGiveaways();
+
+  async function getGiveawayName(id) { // giveaway name
+    try {
+      const ethersProvider = new ethers.providers.Web3Provider(walletProvider)
+      const contract = new ethers.Contract(contractAddress, ABI, ethersProvider);
+      const giveaway = await contract.getGiveaway(id);
+      const name = giveaway["giveawayName"];
+      return name;
+    } catch (e) { console.log(e) }
+  }
+
+  async function getGiveawayAmount(id) { // ödül
+    try {
+      const ethersProvider = new ethers.providers.Web3Provider(walletProvider)
+      const contract = new ethers.Contract(contractAddress, ABI, ethersProvider);
+      const giveaway = await contract.getGiveaway(id);
+      const price = giveaway["amount"];
+      return price._hex;
+    } catch (e) { console.log(e) }
+  }
+
+  async function getGiveawayCreater(id) { // creater
+    try {
+      const ethersProvider = new ethers.providers.Web3Provider(walletProvider)
+      const contract = new ethers.Contract(contractAddress, ABI, ethersProvider);
+      const giveaway = await contract.getGiveaway(id);
+      const owner = giveaway["owner"];
+      return owner;
+    } catch (e) { console.log(e) }
+  }
+
+  async function getGiveawayStartDate(id) { // return start date.
+    try {
+      const ethersProvider = new ethers.providers.Web3Provider(walletProvider)
+      const contract = new ethers.Contract(contractAddress, ABI, ethersProvider);
+      const giveaway = await contract.getGiveaway(id);
+      const date = giveaway["base"]["startDate"]._hex;
+
+      const avaxFujiUnixTimestamp = converToDeciaml(date);
+
+      const timestampInSeconds = avaxFujiUnixTimestamp; // Assuming it's in seconds
+      const timestampInMilliseconds = timestampInSeconds * 1000;
+      const dateObject = new Date(timestampInMilliseconds);
+      
+      const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+      const formattedDate = dateObject.toLocaleDateString('en-US', options);
+
+      return formattedDate;
+      
+    } catch (e) { console.log(e) }
+  }
+
+  async function getGiveawayEndDate(id) { // end date
+    try {
+      const ethersProvider = new ethers.providers.Web3Provider(walletProvider);
+      const contract = new ethers.Contract(contractAddress, ABI, ethersProvider);
+      const giveaway = await contract.getGiveaway(id);
+      const startDate = giveaway.base.startDate._hex;
+      const timeLength = giveaway.timeLength._hex;
+  
+      const startTimestamp = convertToDecimal(startDate);
+      const endTimeInSeconds = startTimestamp + convertToDecimal(timeLength);
+      const endTimeInMilliseconds = endTimeInSeconds * 1000;
+  
+      // Create a Date object for the end time
+      const endDateObject = new Date(endTimeInMilliseconds);
+  
+      // Format the date as a string
+      const options = { day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+      const formattedEndDate = endDateObject.toLocaleDateString('en-US', options);
+  
+      return formattedEndDate;
+    } catch (e) {
+      console.log(e);
+      throw new Error('Failed to get giveaway end date.');
+    }
+  }
+  
+  // Assume convertToDecimal is a function to convert hex to decimal
+  function convertToDecimal(hex) {
+    return parseInt(hex, 16);
+  }
+
+
+
+  // async function getLiveGiveaways() {
+  //   try {
+  //     if (!isConnected || !walletProvider) {
+  //       throw new Error("User not connected or wallet provider not available");
+  //     }
+  
+  //     const ethersProvider = new ethers.providers.Web3Provider(walletProvider);
+  //     const contract = new ethers.Contract(contractAddress, ABI, ethersProvider);
+  //     const liveGiveaways = await contract.getLiveGiveaways();
+  //     console.log(liveGiveaways)
+  //     return liveGiveaways;
+  //   } catch (error) {
+  //     console.error("Error fetching live giveaways:", error);
+  //     // Handle the error, show a message to the user, or take appropriate action
+  //   }
+  // } 
+  // getLiveGiveaways();
 
   // async function getLiveGiveawaysInfo() { // return array of liveaways.
   //   for (let i = 0; i < liveGiveawaysIDS.length; i++) {
@@ -56,10 +155,39 @@ function AllRaffles() {
     return ether;
   }
 
+  const converToDeciaml = (hex) => {
+    const decimal = parseInt(hex, 16);
+    return decimal;
+  }
+
+  const getGiveawayInfo = async (id) => {
+    try {
+      const raffleAmount = converToEthFromHex(await getGiveawayAmount(id));
+      const raffleName = await getGiveawayName(id);
+      const raffleCreater = await getGiveawayCreater(id);
+      const raffleMaxParticipants = await getGiveawayMaxParticuls(id);
+      const raffleStartDate = await getGiveawayStartDate(id);
+      const raffleEndDate = await getGiveawayEndDate(id);
+  
+      return {
+        amount: raffleAmount,
+        name: raffleName,
+        creater: raffleCreater,
+        maxParticipants: raffleMaxParticipants,
+        startDate: raffleStartDate,
+        endDate: raffleEndDate,
+      };
+    } catch (error) {
+      console.error("Error getting giveaway info:", error);
+      return null;
+    }
+  };
+
+  console.log(getGiveawayInfo(0))
 
   const initialRaffles = [
     {
-      raffleImage: image, raffleName: "Azat's 100 ETH GIVEAWAY", rules: 'follow us', status: 'live', prize: "1000 ETH", duration: "7 Days", joined: "1560", creater: "Azat",
+      raffleImage: image, raffleName: raffle1.name, rules: 'follow us', status: 'live', prize: raffle1.amount, duration: raffle1.endDate, joined: "1560", creater: raffle1.creater,
       rules: [
         { title: "You must follow us in twitter.", img: x },
         { title: "There must be at least 5 ETH in your account.", img: wallet },
